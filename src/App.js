c8ea28b919391dc710348f8f8b401494980f2ff0
stefpolanco1 <78278343+stefpolanco1@users.noreply.github.com>
2021-08-07 18:23:17 -0400

    Creation of dictionary app

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        Dictionary
+      </header>
+      <p>Search a word...</p>
+    </div>
+  );
+}
+
+export default App;
